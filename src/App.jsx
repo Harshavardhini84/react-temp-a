cc94e6db7094431bc391bde83c4fa909e2d8954e
Harshavardhini84 <vardhini22241a05a7@grietcollege.com>
2025-03-20 14:11:41 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import App1 from "./components/App1";
 import App2 from "./components/App2";
 import App3 from "./components/App3";
 import App4 from "./components/App4";
+import App5 from "./components/App5";
 
 function App() {
   // const [count, setCount] = useState(0)
@@ -14,10 +15,11 @@ function App() {
   return (
     <BrowserRouter>
     <div>
-      <Link to="app1">App1   | </Link>
+      <Link to="app1"> App1  | </Link>
       <Link to="app2"> App2  | </Link>
       <Link to="app3"> App3  | </Link>
       <Link to="app4"> App4  | </Link>
+      <Link to="app5"> App5  | </Link>
     </div>
     <div>
       <Routes>
@@ -26,6 +28,7 @@ function App() {
         <Route path='app2' element={<App2/>}/>
         <Route path='app3' element={<App3/>}/>
         <Route path='app4' element={<App4/>}/>
+        <Route path='app5' element={<App5/>}/>
       </Routes>
     </div>
     </BrowserRouter>
---
+++ src/components/App5.jsx
@@ -0,0 +1,3 @@
+export default function App5(){
+    return <h1>This is App5</h1>
+}
