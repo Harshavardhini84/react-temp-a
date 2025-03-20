5d4066d21c593e0680ebbe8af37803d31fb6eadd
Harshavardhini84 <vardhini22241a05a7@grietcollege.com>
2025-03-20 14:46:24 +0530

    updated

--- src/components/App5.jsx
+++ src/components/App5.jsx
@@ -1,3 +1,29 @@
-export default function App5(){
-    return <h1>This is App5</h1>
+import { useState } from "react"
+export default function App5() {
+  const [todos, setTodos] = useState([]);
+  const [task, setTask] = useState();
+  const handleSubmit = () => {
+    setTodos([...todos, task]);
+  };
+  const handleDelete =(task) => {
+    setTodos(todos.filter((value)=> task!==value));
+  };
+  return (
+    <div>
+      <h3>This is App5</h3>
+      <p>
+        <input
+          type="text"
+          onChange={(e) => setTask(e.target.value)}
+          placeholder="Enter task"
+        ></input>
+        <button onClick={handleSubmit}>Submit</button>
+        <hr></hr>
+        {todos.map((value, index) => (
+          <li key={index}>{value}
+          {value}<button onClick={() => handleDelete(value)}>Delete</button></li>
+        ))}
+      </p>
+    </div>
+  );
 }
